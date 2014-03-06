b3f0a74b2c7f08e49de16cb49227e562db3901ac
Kevin Florenzano <kevinflo@gmail.com>
2014-03-06 15:02:45 -0800

    Nuked old user model completely to get ready for devise

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,4 @@
 class StaticPagesController < ApplicationController
   def home
-    @user = User.new(user_params)
   end
 end
-
-private
-
-    def user_params
-      params.fetch(:user, {})
-    end
