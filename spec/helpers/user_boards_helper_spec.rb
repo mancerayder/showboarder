63ee8a54e7103fc30cd4a8deab1c77555fdbbc8c
Kevin Florenzano <kevinflo@gmail.com>
2014-03-21 17:48:42 -0700

    generated initial Boards and UserBoards controllers

---
+++ spec/helpers/user_boards_helper_spec.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'
+
+# Specs in this file have access to a helper object that includes
+# the UserBoardsHelper. For example:
+#
+# describe UserBoardsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       helper.concat_strings("this","that").should == "this that"
+#     end
+#   end
+# end
+describe UserBoardsHelper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
