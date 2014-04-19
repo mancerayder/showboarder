6865de0d23c081b7f0be952a132d2df0e55ef84b
Kevin Florenzano <kevinflo@gmail.com>
2014-04-18 19:29:34 -0700

    going to add polymorphic ticket_owner purchasing to support guest and user.  Then ticket ownership transfer upon signup

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,18 +1,59 @@
 class TicketsController < ApplicationController
-  load_and_authorize_resource :board, :find_by => :vanity_url
-  load_and_authorize_resource :show, :through => :board
-  load_and_authorize_resource :ticket, :through => :show
-
   def index
+    @board = Board.find_by_vanity_url(params[:board_id])
     @show = Show.find(params[:show_id])
   end
 
   def reserve
     @show = Show.find(params[:show_id])
-    redirect_to @show
-  end
-end
+    @quantity = params[:quantity]
 
+    if user_signed_in
+      redirect_to current_user
+    else
+      redirect_to
+  end
+
+  def create
+    if params[:charge_type] == "sb3"
+      begin
+      @show = Show.find(params[:show_id])
+      token = params[:stripeToken]
+      quantity = params[:quantity]
+      @amount = @show.price_adv * @quantity
+      @board = Board.find_by_vanity_url(params[:board_id])
+      buyer_id = 0
+      buyer_type = ""
+
+      if current_user.stripe_id
+        customer = Stripe::Customer.retrieve(current_user.stripe_id)
+        charge = Stripe::Charge.create(
+          :customer => customer.id,
+          :amount => @amount,
+          :currency => "usd",
+          :description => @show.id.to_s + " " + quantity.to_s
+          )
+      else
+        customer = Stripe::Customer.create(
+          :card => token,
+          :amount => @amount,
+          :email => current_user.email,
+          :description => "Single show ticketing - new"
+        )
+
+        current_user.update_attributes(stripe_id:customer.id)
+      end
+
+      @show.update_attributes(payer_id:current_user.id, paid_at:Time.now)
+      @show.tickets_make
+
+      redirect_to @show.board, :notice => "You have successfully enabled ticketing for this show!"
+      rescue Stripe::CardError => e
+        flash[:error] = e.message
+        redirect_to board_charges_path(@board)
+      end
+    end
+end
 
   # def create
   #   if params[:charge_type] == "sb3"
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,5 +1,6 @@
 class Ticket < ActiveRecord::Base
   belongs_to :user
+  belongs_to :ticket_owner, polymorphic: true
   belongs_to :show, dependent: :destroy
   belongs_to :referral_band
 
