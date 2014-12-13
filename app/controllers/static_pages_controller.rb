23a10c4db4d246c798b56a0c76fce109942dbdad
Kevin Florenzano <kevinflo@gmail.com>
2014-12-12 16:06:27 -0800

    added rudimentary logged in view for users with no showboard

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,18 +4,10 @@ class StaticPagesController < ApplicationController
       @user = current_user
       if @user.boards.count == 1
         @board = @user.boards.first
-        # @shows = @board.shows.paginate(page: params[:page])
         @shows = @board.shows.where("show_at > ?", Date.tomorrow).order(:show_at).paginate(page: params[:page])
       end
-      # @boards = current_user.boards
-      # if @boards.count > 1
-      #   @boards = @boards.paginate(page: params[:page])
-      # end
+      @tickets_by_show = @user.tickets_by_show
 
-      # if @boards.count == 1
-      #   @board = @boards.first
-      #   @shows = @board.shows.paginate(page: params[:page])
-      # end
       render layout: "application"
     else
       render layout: "landing"
--- app/models/user.rb
+++ app/models/user.rb
@@ -153,34 +153,11 @@ class User < ActiveRecord::Base
     end
   end
 
-  # def self.find_first_by_auth_conditions(warden_conditions)
-  #   conditions = warden_conditions.dup
-  #   if login = conditions.delete(:login)
-  #     where(conditions).where(["lower(name) = :value OR lower(email) = :value", { :value => login.downcase }]).first
-  #   else
-  #     where(conditions).first
-  #   end
-  # end
-
-  # validates :name, presence: true, unique: true, length: { minimum: 4, maximum: 20 }
-  # before_save { email.downcase! }
-  # VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
-  # validates :email, presence:   true,
-  #                   format:     { with: VALID_EMAIL_REGEX },
-  #                   uniqueness: { case_sensitive: false }
-  # validates_inclusion_of :role, in: %w( test beta guest customer member admin ), message: "role %{value} is not predefined"
-  # has_secure_password validations: false
-
-  # with_options unless: :beta_or_guest? do |beta_or_guest|
-  #   beta_or_guest.validates :name, presence: true, unique: true, length: { minimum: 4, maximum: 20 }
-  #   beta_or_guest.validates :password, presence: true, length: { minimum: 6, maximum: 50}
-  #   beta_or_guest.validates_confirmation_of :password, :if => lambda { |m| m.password.present? }
-  #   beta_or_guest.validates_presence_of     :password, :on => :create
-  #   beta_or_guest.validates_presence_of     :password_confirmation, :if => lambda { |m| m.password.present? }
-  #   beta_or_guest.before_create { |r| raise "Password digest missing on new record" if r.password_digest.blank? }
-  # end
-
-  # def beta_or_guest?
-  #   role == "guest" || role == "beta"
-  # end
+  def tickets_by_show
+    tbs = Hash.new { |hash, key| hash[key] = [] }
+    self.tickets.each do |t|
+      tbs[t.show] << t
+    end
+    return tbs
+  end
 end
