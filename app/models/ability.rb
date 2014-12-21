465fd3fbffd08c0b8ea05055a0c547dd98f02fe7
Kevin Flo <kevinflo@gmail.com>
2014-12-20 19:19:11 -0800

    cleaned up permissions a bit

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,22 +9,16 @@ class Ability
       can :manage, :all
     end
 
+    can :read, Board, :state => "public"
+
+    can :read, Show, :state => "public"
+
     can :manage, Board do |board|
       user.user_boards.where(board_id:board.id, role:"manager").length > 0
     end
 
-    # can :crud, Board do |board|
-    #   user.user_boards.where(board_id:board.id, role:"owner").length > 0
-    # end
-
     if user.id
       can :create, Board
-    end
-
-    can :read, Board, :state => "public"
-
-    can :read, :update, Board do |board|
-      user.user_boards.where(board_id:board.id, role:"coordinator").length > 0
     end
 
     can :manage, Board do |board|
@@ -43,55 +37,16 @@ class Ability
       user.user_boards.where(board_id:show.board_id, role:"owner").length > 0
     end
 
-    # can :crud, Show do |show|
-    #     user.user_boards.where(board_id:show.board_id, role:"manager").length > 0
-    # end
+    can :crud, Ticket do |ticket|
+      user.user_boards.where(board_id:ticket.show.board_id, role:"owner").length > 0
+    end
 
-    # can :crud, Show
+    can :crud, Ticket do |ticket|
+      user.user_boards.where(board_id:ticket.show.board_id, role:"manager").length > 0
+    end
 
-    can :read, Show, :state => "public"
-
-    # can :crud, Ticket do |ticket|
-    #   user.user_boards.where(board_id:ticket.show.board.id, role:"manager").length > 0
-    # end
-
-    # can :crud, Ticket do |ticket|
-    #   user.user_boards.where(board_id:ticket.show.board.id, role:"owner").length > 0
-    # end
-
-    can :crud, Ticket
-
-    # can :read, Ticket, :user_id => user.id
-
-    # can :crud, :all
-    # can :manage, :all
-
-
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details:
-    # https://github.com/bryanrite/cancancan/wiki/Defining-Abilities
+    can :crud, Ticket do |ticket|
+      ticket.ticket_owner == user
+    end
   end
 end
