468662c74f921f0d77f6c45533d31e47ee4c9b82
Kevin Flo <kevinflo@gmail.com>
2014-12-30 01:05:40 -0600

    specified s3 hostname to match bucket region

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -95,6 +95,7 @@ Showboarder::Application.configure do
 
   config.paperclip_defaults = {
     storage: :s3,
+    :s3_host_name => 's3-us-west-1.amazonaws.com',
     :s3_protocol => :https,
     s3_credentials: {
       bucket: ENV['AWS_BUCKET'],
