233cea3131c8042048a07050c5126efee354d97b
Kevin Flo <kevinflo@gmail.com>
2015-01-13 01:46:16 -0800

    more csrf changes to fix devise bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   respond_to :html, :json
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
   before_filter :configure_permitted_parameters, if: :devise_controller?
   # check_authorization
 
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  skip_before_filter :verify_authenticity_token, :only => [:create]
+  skip_before_filter :verify_authenticity_token
   def new
     @reserve_code = ""
     @path_from = ""
